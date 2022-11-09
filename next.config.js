6759be4258083ed3eb2567f3edc967143e9c38be
MacBook <air@MacBook-Air-MacBook.local>
2022-11-09 13:25:04 +0200

    some changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+	images: {
+    // loader: "custom",
+    unoptimized: true,
+  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, pages, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     "/": { page: "/" },
+  //     "/resume": { page: "/resume" },
+  //     "/crypto": { page: "/crypto" },
+  //     "/halloween": { page: "/halloween" },
+  //     "/imovie": { page: "/imovie" },
+  //     "/lyricks": { page: "/lyricks" },
+  //     "/modern": { page: "/modern" },
+  //     "/plants": { page: "/plants" },
+  //     "/_error": { page: "/404" },
+  //     "/_error": { page: "/500" },
+  //   };
+  // },
 }
 
 module.exports = nextConfig
